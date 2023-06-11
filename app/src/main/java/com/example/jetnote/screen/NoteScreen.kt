e7d74a7ca9a3a0c0083b8e622b00007330747f9f
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-06-11 15:05:27 +0530

    notes

--- app/src/main/java/com/example/jetnote/screen/NoteScreen.kt
+++ app/src/main/java/com/example/jetnote/screen/NoteScreen.kt
@@ -9,6 +9,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,10 +20,24 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetnote.R
+import com.example.jetnote.components.NoteButton
 import com.example.jetnote.components.NoteInputText
+import com.example.jetnote.model.Note
 
 @Composable
-fun NoteScreen(){
+fun NoteScreen(
+    notes: List<Note>,
+    onAddNote: (Note) -> Unit,
+    onRemoveNote: (Note) -> Unit
+){
+    var title by remember{
+        mutableStateOf("")
+
+    }
+    var description by remember{
+        mutableStateOf("")
+    }
+
     Column(modifier = Modifier.padding(6.dp)) {
         TopAppBar(title = {
                           Text(text = stringResource(id = R.string.app_name))
@@ -32,14 +50,42 @@ fun NoteScreen(){
         //CONTENT
         Column(modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally) {
-            NoteInputText(text = "hello", label = "Hello", onTextChange = {} )
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = title,
+                label = "Title",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace()
+                        }) title = it
+
+                } )
+
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = description,
+                label = "Add a note",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace()
+                        }) description = it
+                })
+
+            NoteButton(text = "Save",
+                onClick = {
+                    if (title.isNotEmpty() && description.isNotEmpty()){
+                        //save/add to the list
+                        title = ""
+                        description = ""
+                    }
+                })
         }
-        
+
     }
 }
  
 @Preview(showBackground = true)
 @Composable
 fun NoteScreenPreview(){
-    NoteScreen()
+    NoteScreen(notes = emptyList(), onAddNote = {}, onRemoveNote = {})
 }
